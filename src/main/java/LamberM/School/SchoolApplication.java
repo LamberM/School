d6f49b9027af26925b348e877842e3dce2c2f09b
LamberM <marcin.lamberski@gmail.com>
2023-06-28 19:28:05 +0200

    update

--- src/main/java/LamberM/School/SchoolApplication.java
+++ src/main/java/LamberM/School/SchoolApplication.java
@@ -2,8 +2,10 @@ package LamberM.School;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("LamberM.School.repository")
 public class SchoolApplication {
 
 	public static void main(String[] args) {
