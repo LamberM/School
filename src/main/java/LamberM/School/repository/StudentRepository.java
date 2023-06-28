df9010523df98e08c820971a1f7bea24c0d65e67
LamberM <marcin.lamberski@gmail.com>
2023-06-28 19:28:35 +0200

    create repository

---
+++ src/main/java/LamberM/School/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package LamberM.School.repository;
+
+import LamberM.School.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+    @Query("SELECT student FROM Student student WHERE student.lastName LIKE %:lastName%")
+    Optional<Student> findByLastName(@Param("lastName") String lastName);
+}
