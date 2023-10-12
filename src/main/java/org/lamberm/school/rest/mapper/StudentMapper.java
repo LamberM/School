82b3787f9de59173a9cf508370077e7380a09c6e
LamberM <marcin.lamberski@gmail.com>
2023-10-12 09:22:07 +0200

    creating StudentDTO with StudentMapper

---
+++ src/main/java/org/lamberm/school/rest/mapper/StudentMapper.java
@@ -0,0 +1,40 @@
+package org.lamberm.school.rest.mapper;
+
+
+import org.lamberm.school.model.Student;
+import org.lamberm.school.rest.model.StudentDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.WARN)
+public interface StudentMapper {
+    default Student map(StudentDTO studentDTO) {
+        if (studentDTO == null) {
+            return null;
+        } else {
+            return Student
+                    .builder()
+                    .id(studentDTO.getId())
+                    .pesel(studentDTO.getPesel())
+                    .firstName(studentDTO.getFirstName())
+                    .secondName(studentDTO.getSecondName())
+                    .lastName(studentDTO.getLastName())
+                    .build();
+        }
+    }
+
+    default StudentDTO map(Student student) {
+        if (student == null) {
+            return null;
+        } else {
+            return StudentDTO
+                    .builder()
+                    .id(student.getId())
+                    .pesel(student.getPesel())
+                    .firstName(student.getFirstName())
+                    .secondName(student.getSecondName())
+                    .lastName(student.getLastName())
+                    .build();
+        }
+    }
+}
