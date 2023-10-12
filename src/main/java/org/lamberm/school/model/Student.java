4e498e5dc6052ac58b5356b0cde63e893d6a985d
LamberM <marcin.lamberski@gmail.com>
2023-10-12 09:22:24 +0200

    changes in Student

--- src/main/java/org/lamberm/school/model/Student.java
+++ src/main/java/org/lamberm/school/model/Student.java
@@ -2,24 +2,32 @@ package org.lamberm.school.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "student")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
     @Id
-//    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-    private Integer number;
+    @Column(name = "PESEL", length = 11, unique = true, nullable = false)
+    private String pesel;
+    @Column(name = "first_name", length = 20, nullable = false)
     private String firstName;
+    @Column(name = "second_name", length = 20)
     private String secondName;
+    @Column(name = "last_name", length = 100, nullable = false)
     private String lastName;
     @Transient
+    private String fullName;
+
     public String getFullName() {
         if (secondName == null) {
             return firstName + " " + lastName;
