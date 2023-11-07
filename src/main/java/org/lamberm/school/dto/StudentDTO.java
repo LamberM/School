3a64ad80fb469625f1d88da365757312fe0c30dc
Marcin Lamberski <marcin.lamberski@gmail.com>
2023-11-07 17:37:13 +0100

    deleted @Builder

--- src/main/java/org/lamberm/school/dto/StudentDTO.java
+++ src/main/java/org/lamberm/school/dto/StudentDTO.java
@@ -1,12 +1,10 @@
 package org.lamberm.school.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentDTO {
