b62e2d7730ce7a613da1e650c2738fd3ceb54a03
Marcin Lamberski <marcin.lamberski@gmail.com>
2023-11-28 08:00:55 +0100

    update

--- src/main/java/org/lamberm/school/teacher/TeacherMapper.java
+++ src/main/java/org/lamberm/school/teacher/TeacherMapper.java
@@ -1,12 +1,17 @@
 package org.lamberm.school.teacher;
 
-import org.mapstruct.*;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface TeacherMapper {
     Teacher map(TeacherDto teacherDto);
 
     TeacherDto map(Teacher teacher);
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateTeacherFromDto(TeacherDto teacherDto, @MappingTarget Teacher teacher);
 }
