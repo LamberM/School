2cf150623ce6e995e9127e6a15f618f5e4c43b62
Marcin Lamberski <marcin.lamberski@gmail.com>
2023-11-28 08:00:29 +0100

    changed mapping

--- src/main/java/org/lamberm/school/teacher/TeacherController.java
+++ src/main/java/org/lamberm/school/teacher/TeacherController.java
@@ -6,42 +6,49 @@ import org.lamberm.school.util.SchoolSubject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v2")
+@RequestMapping("/teachers")
 @RequiredArgsConstructor
 @Validated
 public class TeacherController {
 
     private final TeacherService teacherService;
 
-    @PostMapping("/teachers")
+    @PostMapping
     public ResponseEntity saveTeacher(@Valid @RequestBody TeacherDto teacherDto) {
         teacherService.addTeacher(teacherDto);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @DeleteMapping("/teachers/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity deleteTeacher(@PathVariable("id") Integer id) {
         teacherService.deleteTeacher(id.longValue());
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @PutMapping("/teachers")
+    @PutMapping
     public ResponseEntity editTeacher(@Valid @RequestBody TeacherDto teacherDto) {
         teacherService.editTeacher(teacherDto);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @GetMapping("/teachers")
+    @GetMapping
     public ResponseEntity<List<TeacherDto>> provideTeachers() {
         return ResponseEntity.ok(teacherService.findAllTeachers());
     }
 
-    @GetMapping("/teachers/{schoolSubject}")
+    @GetMapping("/{schoolSubject}")
     public ResponseEntity<List<TeacherDto>> provideTeachersBySchoolSubject(@Valid @PathVariable("schoolSubject") SchoolSubject schoolSubject) {
         return ResponseEntity.ok(teacherService.findTeachersBySchoolSubject(schoolSubject));
     }
