dd5bd03b2896668f56a641d9f1e2d2339cf4bca0
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-01-02 20:29:35 +0100

    created new exceptions

--- src/main/java/org/lamberm/school/error/handler/RestExceptionHandler.java
+++ src/main/java/org/lamberm/school/error/handler/RestExceptionHandler.java
@@ -19,9 +19,26 @@ public class RestExceptionHandler {
     protected ResponseEntity<RestErrorResponse> handleRestClientException(HttpClientErrorException httpClientErrorException) {
         return ResponseEntity.status(httpClientErrorException.getStatusCode()).body(new RestErrorResponse(httpClientErrorException.getStatusCode().value(), httpClientErrorException.getMessage()));
     }
+
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     protected ResponseEntity<RestErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
         return ResponseEntity.badRequest()
                 .body(new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), methodArgumentNotValidException.getMessage()));
+    }
+
+    @ExceptionHandler(value = {StudentClassExistException.class})
+    protected ResponseEntity<RestErrorResponse> handleStudentClassExistException(StudentClassExistException studentClassExistException) {
+        return ResponseEntity.badRequest()
+                .body(new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), studentClassExistException.getMessage()));
+    }
+
+    @ExceptionHandler(value = {StudentClassNotExistException.class})
+    protected ResponseEntity<RestErrorResponse> handleStudentClassNotExistException(StudentClassExistException studentClassExistException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new RestErrorResponse(HttpStatus.NOT_FOUND.value(), studentClassExistException.getMessage()));
+    }
+
+    @ExceptionHandler(value = {TeacherLastNameNotExistException.class})
+    protected ResponseEntity<RestErrorResponse> handleTeacherLastNameNotExistException(TeacherLastNameNotExistException teacherLastNameNotExistException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new RestErrorResponse(HttpStatus.NOT_FOUND.value(), teacherLastNameNotExistException.getMessage()));
     }
 }
